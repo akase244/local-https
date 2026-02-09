e58efdae41f064239eec7ae09e7421ca574a9427
akase244 <akase244@gmail.com>
2026-02-09 23:43:50 +0900

    mkcertによるHTTPSの環境構築例を実装

---
+++ nginx-mkcert/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+CERT_DIR="/etc/nginx/certs"
+CERT_NAME="snakeoil"
+KEY="${CERT_DIR}/${CERT_NAME}.key"
+CRT="${CERT_DIR}/${CERT_NAME}.crt"
+
+export MKCERT_CA_ROOT="/mkcert"
+
+mkdir -p "$CERT_DIR"
+
+if [ ! -f "${KEY}" ] || [ ! -f "${CRT}" ]; then
+  echo "generating certificate..."
+
+  mkcert \
+    -key-file "${KEY}" \
+    -cert-file "${CRT}" \
+    localhost 127.0.0.1
+
+  chmod 600 "${KEY}"
+  chmod 644 "${CRT}"
+else
+  echo "certificate already exists"
+fi
+
+exec "$@"
