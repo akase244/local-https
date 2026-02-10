f54d99868e63005fdffbee261cb7c463a7eebb2d
akase244 <akase244@gmail.com>
2026-02-11 00:35:28 +0900

    通常のサーバー証明書をルート証明書として動作するように修正

--- nginx/README.md
+++ nginx/README.md
@@ -40,30 +40,45 @@ $ docker compose stop
 $ docker compose down
 ```
 
+Dockerコンテナ内で作成されたCA証明書として機能するサーバー証明書をホスト側に登録
+
+```
+$ sudo cp certs/snakeoil.crt /usr/local/share/ca-certificates/nginx_rootCA.crt
+$ ls -l /usr/local/share/ca-certificates/nginx_rootCA.crt
+-rw-r--r-- 1 root root 2122  2月 11 00:08 /usr/local/share/ca-certificates/nginx_rootCA.crt
+$ sudo update-ca-certificates
+```
+
 HTTPアクセスの確認
 
 ```
 $ curl -I http://127.0.0.1/
 HTTP/1.1 301 Moved Permanently
 Server: nginx/1.28.2
-Date: Mon, 09 Feb 2026 15:17:42 GMT
+Date: Tue, 10 Feb 2026 15:08:37 GMT
 Content-Type: text/html
 Content-Length: 169
 Connection: keep-alive
 Location: https://127.0.0.1/
 ```
 
-HTTPSアクセスの確認
+HTTPSアクセスの確認（「-k」、「--insecure」の指定は不要です）
 
 ```
-$ curl -Ik https://127.0.0.1/
+$ curl -I https://127.0.0.1/
 HTTP/1.1 200 OK
 Server: nginx/1.28.2
-Date: Mon, 09 Feb 2026 15:17:55 GMT
+Date: Tue, 10 Feb 2026 15:09:02 GMT
 Content-Type: text/html
 Content-Length: 25
-Last-Modified: Mon, 09 Feb 2026 15:15:07 GMT
+Last-Modified: Mon, 09 Feb 2026 15:26:14 GMT
 Connection: keep-alive
-ETag: "6989f9fb-19"
+ETag: "6989fc96-19"
 Accept-Ranges: bytes
 ```
+
+Google Chromeで「この接続ではプライバシーが保護されません」といった警告が出ないようにするには以下の操作を行います
+
+Google Chromeで `chrome://settings/certificates` にアクセスします
+
+「ローカル証明書」→「カスタム」→「自分でインストール」→「信頼できる証明書」から `certs/snakeoil.crt` をインポートします
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,26 +3,51 @@ set -e
 
 CERT_DIR="/etc/nginx/certs"
 CERT_NAME="snakeoil"
-KEY="${CERT_DIR}/${CERT_NAME}.key"
-CRT="${CERT_DIR}/${CERT_NAME}.crt"
+SERVER_KEY="${CERT_DIR}/${CERT_NAME}.key"
+SERVER_CRT="${CERT_DIR}/${CERT_NAME}.crt"
 
 mkdir -p "$CERT_DIR"
 
-if [ ! -f "${KEY}" ] || [ ! -f "${CRT}" ]; then
+if [ ! -f "${SERVER_KEY}" ] || [ ! -f "${SERVER_CRT}" ]; then
   echo "generating certificate..."
 
-  openssl req \
-    -x509 \
-    -nodes \
-    -days 3650 \
-    -newkey rsa:4096 \
-    -keyout "${KEY}" \
-    -out "${CRT}" \
-    -subj "/C=JP/ST=Tokyo/L=Localhost/O=Snakeoil/OU=Dev/CN=localhost" \
-    -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"
+  # basicConstraints に CA:TRUE が指定されているため、このサーバー証明書はCA証明書として機能します
+  cat > "${CERT_DIR}/openssl.cnf" <<EOF
+[req]
+default_bits = 4096
+prompt = no
+default_md = sha256
+distinguished_name = dn
+x509_extensions = v3_ca
 
-  chmod 600 "${KEY}"
-  chmod 644 "${CRT}"
+[dn]
+C = JP
+ST = Tokyo
+L = Localhost
+O = Snakeoil
+OU = Development
+CN = localhost
+
+[v3_ca]
+subjectAltName = @alt_names
+basicConstraints = critical,CA:TRUE
+keyUsage = critical,digitalSignature,keyEncipherment,keyCertSign
+extendedKeyUsage = serverAuth
+
+[alt_names]
+DNS.1 = localhost
+IP.1 = 127.0.0.1
+IP.2 = ::1
+EOF
+
+  openssl req -x509 -nodes -days 365 \
+    -newkey rsa:4096 \
+    -keyout "${SERVER_KEY}" \
+    -out "${SERVER_CRT}" \
+    -config "${CERT_DIR}/openssl.cnf"
+
+  chmod 600 "${SERVER_KEY}"
+  chmod 644 "${SERVER_CRT}"
 else
   echo "certificate already exists"
 fi
